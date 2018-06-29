3f918c85ec1799b301eee3536976ec8e561f5a97
Dan Elias <daniel@elias-family.com>
2018-06-29 14:53:43 +1000

    modified requirements.txt for ReadTheDocs

--- update_docs.sh
+++ update_docs.sh
@@ -7,5 +7,9 @@ make clean
 make html
 cd ..
 
+# Prepare requirements.txt for ReadThedocs
+cat requirements_doc.txt > requirements.txt
+cat requirements_package.txt >> requirements.txt
+
 # Open docs in browser
 ./open_docs.sh
